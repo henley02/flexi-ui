3a9e8da6347f17d96eb7c28dc7213a1bda783443
Henley <410451447@qq.com>
2024-05-20 22:53:13 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Button from '@/components/Button/Button.vue'
+</script>
+
+<template>
+  <Button type="primary">321312</Button>
+  <Button type="primary" plain>321312</Button>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Button/Button.vue
@@ -0,0 +1,34 @@
+<template>
+  <button
+    class="fu-button"
+    :class="{
+      [`fu-button--${type}`]: type,
+      [`fu-button--${size}`]: size,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-disabled': disabled,
+    }"
+    :disabled="disabled"
+  >
+    <span>
+      <slot />
+    </span>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { defineOptions } from 'vue'
+import type { ButtonProps } from './types'
+
+defineOptions({
+  name: 'FuButton'
+})
+
+defineProps<ButtonProps>()
+
+</script>
+
+<style scoped>
+
+</style>
